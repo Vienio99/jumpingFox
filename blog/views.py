9fe7fbd2bbc462e8e98c30df73b3afa72e94b31d
Vienio99 <kyve96@gmail.com>
2021-01-20 22:35:56 +0100

    Added logging in and signup functionality

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 # Create your views here.
@@ -11,18 +13,31 @@ class BlogListView(generic.ListView):
     template_name = 'home.html'
     context_object_name = 'all_posts'
     
-
 class BlogDetailView(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
     context_object_name = 'post'
 
-class BlogCreateView(generic.CreateView):
+class BlogCreateView(LoginRequiredMixin, generic.CreateView):
     model = Post
     template_name = 'post_new.html'
-    fields = ['author', 'title', 'body']
+    fields = ['title', 'body']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(BlogCreateView, self).form_valid(form)
 
-class BlogUpdateView(generic.UpdateView):
+class BlogUpdateView(UserPassesTestMixin, LoginRequiredMixin, generic.UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class BlogDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
+
